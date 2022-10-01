9a75539f17b7c6119e5f86691b29322e67e85526
nikandfor <nikandfor@gmail.com>
2022-10-02 00:20:33 +0500

    WIP: lower the implementation level

---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/nikandfor/socks5
+
+go 1.19
---
+++ v2/socks5.go
@@ -0,0 +1,374 @@
+package socks5
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"strconv"
+)
+
+type (
+	AuthMethod uint8
+
+	// Command is request command or reply code
+	Command uint8
+
+	// Addr is domain name addr
+	Addr string
+
+	Proto struct{}
+)
+
+// Auth methids.
+const (
+	AuthNone AuthMethod = iota
+	AuthGSSAPI
+	AuthUserPass
+	AuthChallengeHandshake
+	_
+	AuthChallengeResponse
+	AuthSSL
+	AuthNDS
+	AuthMultiAuthFramework
+	AuthJSON
+	AuthN
+
+	AuthNoAcceptable AuthMethod = 0xff
+)
+
+// Commands
+const (
+	CommandTCPConn  Command = 0x01
+	CommandTCPBind  Command = 0x02
+	CommandUDPAssoc Command = 0x03
+)
+
+// Reply Codes
+const (
+	ReplySuccess Command = iota
+	ReplyGeneralFailure
+	ReplyNotAllowed
+	ReplyNetworkUnreachable
+	ReplyHostUnreachable
+	ReplyConnRefused
+	ReplyTTLExpired
+	ReplyCommandNotSupported
+	ReplyAddressTypeNotSupported
+)
+
+// errors
+var (
+	ErrUnsupportedProtocol    = errors.New("unsupported protocol")
+	ErrUnsupportedCommand     = errors.New("unsupported command")
+	ErrUnsupportedAddressType = errors.New("unsupported address type")
+	ErrNoAcceptableAuth       = errors.New("no acceptable auth method")
+)
+
+func (p Proto) ClientHandshake(c net.Conn, methods ...AuthMethod) (auth AuthMethod, err error) {
+	var buf [257]byte
+
+	if len(methods) > 255 {
+		return 0, fmt.Errorf("too much of auth methods")
+	}
+
+	buf[0] = 0x5
+	buf[1] = byte(len(methods))
+
+	for i, m := range methods {
+		buf[2+i] = byte(m)
+	}
+
+	_, err = c.Write(buf[:2+len(methods)])
+	if err != nil {
+		return 0, err
+	}
+
+	_, err = io.ReadFull(c, buf[:2])
+	if err != nil {
+		return 0, err
+	}
+
+	if buf[0] != 0x5 {
+		return 0, ErrUnsupportedProtocol
+	}
+
+	auth = AuthMethod(buf[1])
+
+	if auth == AuthNoAcceptable {
+		return auth, ErrNoAcceptableAuth
+	}
+
+	return auth, nil
+}
+
+func (p Proto) ServerHandshake(c net.Conn, methods ...AuthMethod) (auth AuthMethod, err error) {
+	var buf [256]byte
+
+	_, err = io.ReadFull(c, buf[:2])
+	if err != nil {
+		return 0, err
+	}
+
+	if buf[0] != 0x5 {
+		return 0, ErrUnsupportedProtocol
+	}
+
+	n, err := io.ReadFull(c, buf[:buf[1]])
+	if err != nil {
+		return 0, err
+	}
+
+	auth = AuthNoAcceptable
+
+authloop:
+	for _, m := range methods {
+		for _, q := range buf[:n] {
+			if m == AuthMethod(q) {
+				auth = m
+
+				break authloop
+			}
+		}
+	}
+
+	buf[0] = 5
+	buf[1] = byte(auth)
+
+	_, err = c.Write(buf[:2])
+	if err != nil {
+		return 0, err
+	}
+
+	if auth == AuthNoAcceptable {
+		return 0, ErrNoAcceptableAuth
+	}
+
+	return auth, nil
+}
+
+func (p Proto) ReadReqRep(c net.Conn) (cmd Command, addr net.Addr, err error) {
+	var buf [258]byte
+
+	_, err = io.ReadFull(c, buf[:4])
+	if err != nil {
+		return 0, nil, err
+	}
+
+	if buf[0] != 0x5 {
+		return 0, nil, ErrUnsupportedProtocol
+	}
+
+	cmd = Command(buf[1])
+
+	addr, err = readAddr(c, cmd, buf[3], buf[:])
+	if err != nil {
+		return cmd, addr, err
+	}
+
+	return cmd, addr, nil
+}
+
+func (p Proto) WriteReqRep(c net.Conn, cmd Command, addr net.Addr) (err error) {
+	var buf [256 + 6]byte
+
+	buf[0] = 0x5
+	buf[1] = byte(cmd)
+	buf[2] = 0
+
+	i := 3
+
+	i, err = encodeAddr(buf[:], i, addr)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.Write(buf[:i])
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func encodeAddr(buf []byte, st int, a net.Addr) (i int, err error) {
+	i = st
+
+	switch a := a.(type) {
+	case *net.TCPAddr:
+		i, err = encodeIP(buf, st, a.IP)
+		if err != nil {
+			return 0, err
+		}
+
+		buf[i] = byte(a.Port >> 8)
+		i++
+		buf[i] = byte(a.Port)
+		i++
+	case *net.UDPAddr:
+		i, err = encodeIP(buf, st, a.IP)
+		if err != nil {
+			return 0, err
+		}
+
+		buf[i] = byte(a.Port >> 8)
+		i++
+		buf[i] = byte(a.Port)
+		i++
+	case Addr:
+		i, err = encodeAddrString(buf, st, string(a))
+		if err != nil {
+			return 0, err
+		}
+	case nil:
+		i, err = encodeAddrString(buf, st, "")
+		if err != nil {
+			return 0, err
+		}
+	default:
+		return 0, fmt.Errorf("unsupported address: %v", a)
+	}
+
+	return i, nil
+}
+
+func encodeIP(buf []byte, st int, ip net.IP) (i int, err error) {
+	i = st
+	q := ip.To4()
+
+	if q != nil {
+		buf[i] = 0x1
+		i++
+
+		i += copy(buf[i:], q)
+	} else if ip.To16() != nil {
+		buf[i] = 0x4
+		i++
+
+		i += copy(buf[i:], ip)
+	} else {
+		return 0, fmt.Errorf("bad ip: %v", ip)
+	}
+
+	return i, nil
+}
+
+func encodeAddrString(buf []byte, st int, addr string) (i int, err error) {
+	i = st
+
+	if addr == "" {
+		// type
+		buf[i] = 0x1 // ipv4
+		i++
+
+		// ipv4 + port
+		i += copy(buf[i:], []byte{0, 0, 0, 0, 0, 0})
+
+		return i, nil
+	}
+
+	host, port, err := net.SplitHostPort(addr)
+	if err != nil {
+		return 0, err
+	}
+
+	if len(host) > 256 {
+		return 0, fmt.Errorf("too long hostname")
+	}
+
+	porti, err := strconv.ParseInt(port, 10, 16)
+	if err != nil {
+		return 0, err
+	}
+
+	buf[i] = 0x3
+	i++
+	buf[i] = byte(len(host))
+	i++
+
+	i += copy(buf[i:], host)
+
+	buf[i] = byte(porti >> 8)
+	i++
+	buf[i] = byte(porti)
+	i++
+
+	return i, nil
+}
+
+func readAddr(c net.Conn, cmd Command, typ byte, buf []byte) (addr net.Addr, err error) {
+	switch typ {
+	case 0x01: // ipv4
+		return readIP(c, cmd, 4, buf)
+	case 0x04: // ipv6
+		return readIP(c, cmd, 16, buf)
+	case 0x03: // domain name
+		return readName(c, buf)
+	default:
+		return nil, fmt.Errorf("%w: %v", ErrUnsupportedAddressType, typ)
+	}
+}
+
+func readIP(c net.Conn, cmd Command, len int, buf []byte) (addr net.Addr, err error) {
+	_, err = io.ReadFull(c, buf[:len+2])
+	if err != nil {
+		return nil, err
+	}
+
+	ip := make([]byte, len)
+
+	copy(ip, buf)
+
+	port := int(buf[len])<<8 | int(buf[len+1])
+
+	switch cmd {
+	case CommandTCPConn, CommandTCPBind:
+		addr = &net.TCPAddr{
+			IP:   ip,
+			Port: port,
+		}
+	case CommandUDPAssoc:
+		addr = &net.UDPAddr{
+			IP:   ip,
+			Port: port,
+		}
+	default:
+		panic(cmd)
+	}
+
+	return addr, nil
+}
+
+func readName(c net.Conn, buf []byte) (addr net.Addr, err error) {
+	_, err = c.Read(buf[:1])
+	if err != nil {
+		return nil, err
+	}
+
+	n := buf[0]
+
+	_, err = io.ReadFull(c, buf[:n+2])
+	if err != nil {
+		return nil, err
+	}
+
+	port := int(buf[n])<<8 | int(buf[n+1])
+
+	return Addr(fmt.Sprintf("%s:%d", buf[:n], port)), nil
+}
+
+func (c Command) String() string {
+	switch c {
+	case CommandTCPConn:
+		return "tcp_connect"
+	case CommandTCPBind:
+		return "tcp_bind"
+	case CommandUDPAssoc:
+		return "udp_assoc"
+	default:
+		return fmt.Sprintf("cmd[%x]", int(c))
+	}
+}
+
+func (a Addr) Network() string { return "" }
+func (a Addr) String() string  { return string(a) }
