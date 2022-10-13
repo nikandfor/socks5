b51089778f45899cd5a8b751321cf7a65caca75f
nikandfor <nikandfor@gmail.com>
2022-10-14 01:58:22 +0500

    user-pass client auth

--- v2/auth.go
+++ v2/auth.go
@@ -53,3 +53,36 @@ func (a UserPassAuth) WriteReply(c net.Conn, status int) (err error) {
 
 	return err
 }
+
+func (a UserPassAuth) WriteRequest(c net.Conn, usr, pwd string) (err error) {
+	if len(usr) > 255 || len(pwd) > 255 {
+		return errors.New("too long user or password")
+	}
+
+	var buf [256]byte
+
+	b := buf[:0]
+	b = append(b, 0x1, byte(len(usr)))
+	b = append(b, usr...)
+	b = append(b, byte(len(pwd)))
+	b = append(b, pwd...)
+
+	_, err = c.Write(b)
+
+	return err
+}
+
+func (a UserPassAuth) ReadReply(c net.Conn) (status int, err error) {
+	var buf [2]byte
+
+	_, err = io.ReadFull(c, buf[:])
+	if err != nil {
+		return -1, err
+	}
+
+	if buf[0] != 0x1 {
+		return -1, errors.New("unsupported version")
+	}
+
+	return int(buf[1]), nil
+}
